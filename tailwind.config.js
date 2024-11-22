9701c79bbd85b46f28534692f672293c63488230
Jonathan Sarry <jsarry@gmail.com>
2024-11-22 17:22:10 +0000

    Install maryui

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,15 @@ import forms from '@tailwindcss/forms';
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        "./vendor/wireui/wireui/src/*.php",
-        "./vendor/wireui/wireui/ts/**/*.ts",
-        "./vendor/wireui/wireui/src/WireUi/**/*.php",
-        "./vendor/wireui/wireui/src/Components/**/*.php",
-    ],
+		'./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+		 './storage/framework/views/*.php',
+		 './resources/views/**/*.blade.php',
+		 "./vendor/wireui/wireui/src/*.php",
+		 "./vendor/wireui/wireui/ts/**/*.ts",
+		 "./vendor/wireui/wireui/src/WireUi/**/*.php",
+		 "./vendor/wireui/wireui/src/Components/**/*.php",
+		 "./vendor/robsontenorio/mary/src/View/Components/**/*.php"
+	],
     presets: [
         require("./vendor/wireui/wireui/tailwind.config.js")
     ],
@@ -24,5 +25,8 @@ export default {
         },
     },
 
-    plugins: [forms],
+    plugins: [
+		forms,
+		require("daisyui")
+	],
 };
