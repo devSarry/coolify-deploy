127b795131ddf007920063e1e17a0ef743491709
Jonathan Sarry <jsarry@gmail.com>
2024-11-22 17:14:51 +0000

    Add process-compose.yaml to TUI and devbox services up command

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,23 +1,11 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Document</title>
+</head>
+<body>
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
-        <!-- Styles -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="antialiased font-sans">
-       <head>
-           <wireui:scripts />
-
-           <script src="//unpkg.com/alpinejs" defer></script>
-       </head>
-    </body>
+</body>
 </html>
