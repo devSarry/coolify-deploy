a858b2affd101a4aeb999dce2955128565463e98
Jonathan Sarry <jsarry@gmail.com>
2024-11-23 19:07:01 +0000

    Add brand color

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,55 +17,11 @@
     <body class="font-sans antialiased">
 
     {{-- The navbar with `sticky` and `full-width` --}}
-    <x-mary-nav sticky full-width>
-
-        <x-slot:brand>
-            {{-- Drawer toggle for "main-drawer" --}}
-            <label for="main-drawer" class="lg:hidden mr-3">
-                <x-mary-icon name="o-bars-3" class="cursor-pointer" />
-            </label>
-
-            {{-- Brand --}}
-            <div>App</div>
-        </x-slot:brand>
-
-        {{-- Right side actions --}}
-        <x-slot:actions>
-            <x-mary-button label="Messages" icon="o-envelope" link="###" class="btn-ghost btn-sm" responsive />
-            <x-mary-button label="Notifications" icon="o-bell" link="###" class="btn-ghost btn-sm" responsive />
-        </x-slot:actions>
-    </x-mary-nav>
+    <livewire:layout.navigation />
 
     {{-- The main content with `full-width` --}}
     <x-mary-main with-nav full-width>
 
-        {{-- This is a sidebar that works also as a drawer on small screens --}}
-        {{-- Notice the `main-drawer` reference here --}}
-        <x-slot:sidebar drawer="main-drawer" collapsible class="bg-base-200">
-
-            {{-- User --}}
-            @if($user = auth()->user())
-                <x-mary-list-item :item="$user" value="name" sub-value="email" no-separator no-hover class="pt-2">
-                    <x-slot:actions>
-                        <x-mary-button icon="o-power" class="btn-circle btn-ghost btn-xs" tooltip-left="logoff" no-wire-navigate link="/logout" />
-                    </x-slot:actions>
-                </x-mary-list-item>
-
-                <x-mary-menu-separator />
-            @endif
-
-            {{-- Activates the menu item when a route matches the `link` property --}}
-            <x-mary-menu activate-by-route>
-                <x-mary-menu-item title="Home" icon="o-home" link="###" />
-                <x-mary-menu title="Messages" icon="o-envelope" link="###" />
-                <x-mary-menu-sub title="Settings" icon="o-cog-6-tooth">
-                    <x-mary-menu-item title="Wifi" icon="o-wifi" link="####" />
-                    <x-mary-menu-item title="Archives" icon="o-archive-box" link="####" />
-                </x-mary-menu-sub>
-            </x-mary-menu>
-        </x-slot:sidebar>
-
-        {{-- The `$slot` goes here --}}
         <x-slot:content>
             {{ $slot }}
         </x-slot:content>
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,28 @@ export default {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
         },
+        colors: {
+            'navbar': '#fce969',
+            "brand": {
+                50: "#FFFDF0",
+                100: "#FEFAE1",
+                200: "#FEF6C3",
+                300: "#FDF1A5",
+                400: "#FDED87",
+                500: "#FCE969",
+                600: "#FBDE23",
+                700: "#D2B704",
+                800: "#8C7A03",
+                900: "#463D01",
+                950: "#231E01"
+            },
+            "brand-yellow": "#fce969",
+            "brand-cyan": "#588B8B",
+            "brand-tangerine": "#F28F3B",
+            "brand-jasper": "#C8553D",
+            "brand-purple": "E8D7FF"
+
+        }
     },
 
     plugins: [
