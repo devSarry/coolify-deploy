f53959f9f3c649c042bf74f5d744c1b30d57140c
Jonathan Sarry <jsarry@gmail.com>
2025-01-05 16:19:32 +0000

    add landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,24 +3,24 @@
         <div class="text-center">
             <h1 class="text-4xl font-bold text-primary mb-4">Never Miss a Movie Night Again</h1>
             <p class="text-lg text-neutral-content mb-8">Plan your movie schedules, share with friends, and enjoy!</p>
-            <x-mary-button label="Get Started" class="btn-primary"/>
+            <x-mary-button link="{{ route('register') }}" label="Get Started" class="btn-primary mr-4"/>
+            @guest
+                <x-mary-button link="{{ route('login') }}" label="Login" class="btn-base"/>
+            @endguest
+
         </div>
     </section>
 
     <section class="container mx-auto px-4 py-16">
-        <p class="text-neutral-content">Easily create and manage your movie schedule with our intuitive interface.</p>
+        <div class="mockup-browser bg-base-300 border">
+            <div class="mockup-browser-toolbar">
+            </div>
+            <picture>
+                <source media="(min-width:650px)" srcset="{{ Vite::asset('resources/images/screen-shot-program.png') }}">
+                <source media="(min-width:465px)" srcset="{{ Vite::asset('resources/images/screen-shot-program.png') }}">
+                <img src="{{ Vite::asset('resources/images/screen-shot-program.png') }}" alt="Flowers" style="width:auto;">
+            </picture>
+        </div>
     </section>
 
-    <section class="container mx-auto px-4 py-16">
-        <p class="text-neutral-content">Easily create and manage your movie schedule with our intuitive interface.</p>
-    </section>
-
-    <section class="container mx-auto px-4 py-16">
-        <p class="text-neutral-content">Easily create and manage your movie schedule with our intuitive interface.</p>
-    </section>
-
-    <section class="container mx-auto px-4 py-16">
-        <h2 class="text-3xl font-bold text-primary mb-4">Ready to start planning your movie nights?</h2>
-        <x-mary-button label="Create Your Program" class="btn-primary"/>
-    </section>
 </x-app-layout>
